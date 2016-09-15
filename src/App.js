0b15db53eaa4d1fe6ffee6aaac3b8cfc2b8c5613
jtaylor1991 <james.taylorii91@gmail.com>
2016-09-15 16:23:54 -0400

    completed exercise 1: handling events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import './normalize.css';
 import './skeleton.css';
+import './App.css';
 
 class messagesApp extends Component {
 
@@ -15,20 +15,52 @@ class messagesApp extends Component {
           {text: 'I am the second message', time: '12:00pm', user: 'Kobe'},
           {text: 'I am the third message', time: '2:00pm', user: 'Lebron'},
           {text: 'I am the third message', time: '2:00pm', user: 'Lebron'}
-        ]
+        ],
+        newMessage: null,
+        editMessage: false
     }
   }
 
+   _updateMessage = (e) => {
+    this.setState({newMessage: e.target.value})
+   }
+
+   _editMessage = () => {
+    this.setState({editMessage: true})
+   }
+
+  _addMessage = (e) => {
+    e.preventDefault();
+    const messageInput =  document.getElementById('messageInput')
+
+    if (messageInput.value === '' || null) { 
+
+      messageInput.classList.add('error')
+      alert('Please Enter A Message')
+    } else {
+
+      messageInput.classList.remove('error')
+      let messages = this.state.messages.slice()
+      messages.push({text: this.state.newMessage})
+      this.setState({messages})
+      messageInput.value = null
+  }
+}
+
   render() {
-    let currentUser = 'Kobe';
-    let messages = this.state.messages.map(function (message, i) {
-    let classes = 'message';
-       if (message.user === currentUser) {
-        classes += ' currentUser';
-       }
-      return (
-          <p className={classes} key={i}> Here is the body of the message {message.text} <br />  The message was posted at {message.time} <br />  The user who posted this message was {message.user} .</p>
-        )
+    const currentUser = 'Kobe';
+    const messages = this.state.messages.map(function (message, i) {
+      let classes = message.user === currentUser ? 'message currentUser' : 'message';
+        if (message.user === currentUser) {
+          return (
+            <p className={classes} key={i}> Hey I'm message # {i}. Here is my contents: {message.text} <br/><button className='button-primary'>Edit Message</button></p>
+          )
+        }else {
+          return (
+            <p className={classes} key={i}> Hey I'm message # {i}. Here is my contents: {message.text}</p>
+          )
+        }
+        
     });
 
  
@@ -37,9 +69,11 @@ class messagesApp extends Component {
 
     return (
       <div className='container'>
+        <form onSubmit={this._addMessage}>
         <label htmlFor='messageInput'>Message Input</label>
-        <input type='text' name='messageInput' id='messageInput'/><br />
-        <button className='primary'>Send</button>
+        <input type='text' name='messageInput' value={this.newMessage} onChange={this._updateMessage} id='messageInput'/><br />
+        <button type="submit" className='primary'>Add Message</button>
+        </form>
         <p><u>Message List</u></p>
         <div>{messages}</div>
          {(function () {
